87c11c472f2dc6a9c3c539f2563e9a678f02d16a
Christopher Rooney <christopherrooney29@gmail.com>
2024-11-26 17:25:02 +0000

    Added demo_lotto

---
+++ Chris Demos/demo_lotto.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+#Author: Chris Rooney
+#Version: 1.0
+#Description: This script will generate 6 unique random lottery numbers
+import random
+
+lotto = []
+
+while len(lotto) < 6:
+    num = random.randint(1, 50)
+    if num not in lotto:
+        lotto.append(num)
+    else:
+        print("Duplicate number =", num)
+
+print("Lottery number =", lotto)
