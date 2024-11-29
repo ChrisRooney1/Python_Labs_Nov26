988024dc5c7035157f0860e880cf56e414853cb3
Christopher Rooney <christopherrooney29@gmail.com>
2024-11-29 16:47:48 +0000

    Added test_unittests

--- Chris Demos/calculator/basic.py
+++ Chris Demos/calculator/basic.py
@@ -8,7 +8,12 @@
 import sys
 
 def add(*args):
-    """ Return sum of all arguments """
+    """ Return sum of all arguments
+    >>> add(4, 3)
+    7
+    >>> add(4, 3, 2)
+    9
+    """
     total = 0
     for num in args:
         total += num
@@ -19,12 +24,24 @@ def sub(x,z):
     return x - z
 
 def mul(*args):
-    """ Return the product of all the arguments"""
+    """ Return the product of all the arguments
+    >>> mul(4, 3)
+    12
+    >>> mul(4, 3, 2)
+    24
+    """
     total = 1
     for num in args:
         total *= num
     return total
 
 def div(x, z):
-    """ Return quotient of x divided by z to 3 decimal places """
+    """ Return quotient of x divided by z to 3 decimal places 
+    >>> div(4, 3)
+    1.333
+    """
     return round(x/z, 3)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
